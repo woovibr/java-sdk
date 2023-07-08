adce20eecedd2a0ddade75c7f2a271dfc9b52f8d
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2023-07-08 20:41:51 +0000

    style: add editorconfig

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,26 @@
 plugins {
-    id("java")
+  id("java")
 }
 
 group = "dev.algebraic"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 dependencies {
-    implementation(platform("com.squareup.okhttp3:okhttp-bom:4.10.0"))
+  implementation(platform("com.squareup.okhttp3:okhttp-bom:4.10.0"))
 
-    implementation("com.squareup.okhttp3:okhttp")
-    implementation("com.squareup.okhttp3:logging-interceptor")
+  implementation("com.squareup.okhttp3:okhttp")
+  implementation("com.squareup.okhttp3:logging-interceptor")
 
-    testImplementation("com.squareup.okhttp3:mockwebserver:4.10.0")
+  testImplementation("com.squareup.okhttp3:mockwebserver:4.10.0")
 
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+  testImplementation(platform("org.junit:junit-bom:5.9.1"))
+  testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
-    useJUnitPlatform()
+  useJUnitPlatform()
 }
