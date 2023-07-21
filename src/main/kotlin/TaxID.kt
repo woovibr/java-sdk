8282a043de8c7f64b141eb6399dc73ba426b1a70
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2023-07-21 14:38:23 +0000

    feat: add docs for the main java code

--- src/main/kotlin/TaxID.kt
+++ src/main/kotlin/TaxID.kt
@@ -7,13 +7,16 @@ import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
+/**
+ * Represents a taxation identifier, such as CPF or CNPJ in Brazil.
+ *
+ * @property text The taxation identifier.
+ * @property kind The taxation identifier kind.
+ */
 @Serializable
 public data class TaxID(
-  @SerialName("taxID")
-  public val text: String,
-
-  @SerialName("type")
-  public val kind: TaxIDKind? = null,
+  @SerialName("taxID") public val text: String,
+  @SerialName("type") public val kind: TaxIDKind? = null,
 ) {
   public fun kind(): TaxIDKind = kind ?: TaxIDKind.CPF
 }
