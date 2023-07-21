3a401c045ad3f69a9ac6a1ec75ab27d6f54a9fe0
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2023-07-21 16:22:43 -0300

    feat: add transactions api (#26)
    * feat: add endpoint to find all transactions
    
    * feat: add endpoint to find single transaction
    
    * style: make ktlint happy

---
+++ src/main/kotlin/Transaction.kt
@@ -0,0 +1,113 @@
+package br.com.openpix.sdk
+
+import io.ktor.client.call.*
+import io.ktor.client.request.*
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+public data class Transaction @JvmOverloads public constructor(
+  public val charge: Charge? = null,
+  public val value: Int,
+  public val time: String,
+  public val payer: Customer? = null,
+  public val globalID: String? = null,
+  public val pixQrCode: PixQrCode? = null,
+
+  public val withdraw: PixWithdrawTransaction? = null,
+
+  @SerialName("endToEndID")
+  public val firstEndToEndId: String? = null,
+
+  @SerialName("endToEndId")
+  public val secondEndToEndId: String? = null,
+
+  @SerialName("infoPagador")
+  public val infoPayer: String? = null,
+
+  @SerialName("customer")
+  public val customer: Customer? = null,
+
+  @SerialName("type")
+  public val kind: TransactionKind,
+)
+
+@Serializable
+public enum class TransactionKind {
+  PAYMENT,
+  WITHDRAW,
+  REFUND,
+  FEE,
+  GIFTBACK,
+}
+
+@Serializable
+public data class TransactionListResponse(
+  public val status: String,
+  public val transactions: List<Transaction>,
+  public val pageInfo: PageInfo,
+)
+
+@Serializable
+public data class TransactionResponse(public val transaction: Transaction)
+
+@Serializable
+public data class PixWithdrawTransaction @JvmOverloads public constructor(
+  public val value: Int? = null,
+  public val time: String? = null,
+  public val payer: Customer? = null,
+
+  @SerialName("type")
+  public val kind: String,
+
+  @SerialName("infoPagador")
+  public val infoPayer: String? = null,
+
+  @SerialName("endToEndID")
+  public val firstEndToEndId: String? = null,
+
+  @SerialName("endToEndId")
+  public val secondEndToEndId: String? = null,
+)
+
+@JvmSynthetic
+public suspend fun WooviSDK.getTransaction(id: String): TransactionResponse {
+  return client.get("/api/v1/transaction/$id").body<TransactionResponse>()
+}
+
+@JvmSynthetic
+public suspend fun WooviSDK.transactions(
+  /** Start date used in the query. Complies with RFC 3339. */
+  start: String? = null,
+
+  /** End date used in the query. Complies with RFC 3339. */
+  end: String? = null,
+
+  /**
+   * You can use the charge ID or correlation ID or transaction ID of charge to get a list of transactions
+   * related of this transaction
+   */
+  charge: String? = null,
+
+  /**
+   * You can use the QrCode static ID or correlation ID or identifier field of QrCode static to get a list
+   * of QrCode related of this transaction
+   */
+  pixQrCode: String? = null,
+
+  /**
+   *You can use the ID or EndToEndId of a withdrawal transaction to get all transactions related to the
+   * withdrawal
+   */
+  withdrawal: String? = null,
+): TransactionListResponse {
+  return client
+    .get("/api/v1/transaction") {
+      start?.let { parameter("start", it) }
+      end?.let { parameter("end", it) }
+      charge?.let { parameter("charge", it) }
+      pixQrCode?.let { parameter("pixQrCode", it) }
+      withdrawal?.let { parameter("withdrawal", it) }
+    }
+    .body<TransactionListResponse>()
+}
--- src/main/kotlin/WooviSDK.kt
+++ src/main/kotlin/WooviSDK.kt
@@ -23,12 +23,6 @@ import kotlinx.coroutines.future.future
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 
-@JvmSynthetic
-public suspend fun main() {
-  val sdk = WooviSDK(appId = System.getenv("APP_ID"))
-  println(sdk.deleteWebhook("V2ViaG9vazo2NDg3NzNjY2JlYmNjZTI0NzQ3ZmIwZGM="))
-}
-
 /**
  * The entrypoint of Woovi SDK for Java, Kotlin and another JVM Languages, it does take an authorization [appId].
  */
@@ -281,6 +275,58 @@ public class WooviSDK @JvmOverloads public constructor(
    */
   public fun createWebhookAsync(builder: WebhookBuilder): Future<WebhookResponse> = future {
     createWebhook(builder) {}
+  }
+
+  /**
+   * Returns a transaction.
+   *
+   * @param id The transaction id.
+   * @return The transaction response.
+   */
+  public fun getTransactionAsync(id: String): Future<TransactionResponse> = future {
+    getTransaction(id)
+  }
+
+  /**
+   * Returns the transactions
+   *
+   * @param start Start date used in the query. Complies with RFC 3339.
+   * @param end End date used in the query. Complies with RFC 3339.
+   * @param charge You can use the charge ID or correlation ID or transaction ID of charge to get a list
+   *  of transactions related of this transaction
+   * @param pixQrCode You can use the QrCode static ID or correlation ID or identifier field of QrCode
+   *  static to get a list of QrCode related of this transaction
+   * @param withdrawal You can use the ID or EndToEndId of a withdrawal transaction to get all transactions
+   *  related to the withdrawal
+   * @return The transaction list response.
+   */
+  @JvmOverloads
+  public fun transactionsAsync(
+    /** Start date used in the query. Complies with RFC 3339. */
+    start: String? = null,
+
+    /** End date used in the query. Complies with RFC 3339. */
+    end: String? = null,
+
+    /**
+     * You can use the charge ID or correlation ID or transaction ID of charge to get a list of transactions
+     * related of this transaction
+     */
+    charge: String? = null,
+
+    /**
+     * You can use the QrCode static ID or correlation ID or identifier field of QrCode static to get a list
+     * of QrCode related of this transaction
+     */
+    pixQrCode: String? = null,
+
+    /**
+     *You can use the ID or EndToEndId of a withdrawal transaction to get all transactions related to the
+     * withdrawal
+     */
+    withdrawal: String? = null,
+  ): Future<TransactionListResponse> = future {
+    transactions(start, end, charge, pixQrCode, withdrawal)
   }
 
   /**
